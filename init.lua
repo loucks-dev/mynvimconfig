f6ff1a6e017d2fe30e97fe3bbd92e52a8cf6df6d
loucks-dev <parkerloucks74@gmail.com>
2025-11-26 20:02:03 -0600

    Lazy configured and plugins ready to add, colorscheme.lua added to test

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 require("config")
 require("config.lazy")
-print("everything initialized successfully")
+print("initialized successfully")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return { 
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    transparent = true,
+    style = "storm",
+    terminal_colors = true,
+    opts = {},
+  }
+}
