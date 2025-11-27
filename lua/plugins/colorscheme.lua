4bf0247745a5405bb6d1d1616d0d9c17e8ed9ec2
loucks-dev <parkerloucks74@gmail.com>
2025-11-26 22:44:39 -0600

    Commenting out colorscheme.lua a bit for clarity in colorscheme swapping

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "folke/tokyonight.nvim", -- must change if swapping colorscheme !! 
     lazy = false,          -- load immediately
     priority = 1000,       
     opts = {
       transparent = true,
-      style = "night",
+      style = "night",  -- depends on colorscheme you're using
       terminal_colors = true,
     },
     config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd("colorscheme tokyonight")
+      require("tokyonight").setup(opts) -- must change if swapping colorscheme !!
+      vim.cmd("colorscheme tokyonight") -- must change if swapping colorscheme !!
     end,
   }
 }
