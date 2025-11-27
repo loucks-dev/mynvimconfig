91b020388906014bfe2c5bb0da387fe424bbbd4d
loucks-dev <parkerloucks74@gmail.com>
2025-11-26 23:09:42 -0600

    Simplifying colorscheme defaulting to use init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("config")
 require("config.lazy")
+vim.cmd("colorscheme tokyonight-moon")
 print("initialized successfully")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,30 @@
 return {
-  {
-    "folke/tokyonight.nvim", -- must change if swapping colorscheme !! 
-    lazy = false,          -- load immediately
-    priority = 1000,       
-    opts = {
-      transparent = true,
-      style = "night",  -- depends on colorscheme you're using
-      terminal_colors = true,
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts) -- must change if swapping colorscheme !!
-      vim.cmd("colorscheme tokyonight") -- must change if swapping colorscheme !!
-    end,
-  }
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,          
+		priority = 1000,
+		opts = {
+			transparent = true,
+		}
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = true,
+		opts = {
+			disable_background = true,
+			disable_float_background = true,
+			disable_italics = true,
+		},
+	},
+	{
+		"navarasu/onedark.nvim",
+		name = "onedark",
+		lazy = true,
+		opts = {
+			style = "darker", -- dark, darker, cool, warm, deep
+			transparent = true,
+		},
+	},
+	-- add more here
 }
