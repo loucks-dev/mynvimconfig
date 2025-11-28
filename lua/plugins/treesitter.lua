8ab8d22c27f41d6d811489c3b783090bd0b242a7
loucks-dev <parkerloucks74@gmail.com>
2025-11-28 00:40:14 -0600

    Treesitter configured

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,24 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-	}
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",      -- update parsers on install
+    lazy = false,             -- load immediately at startup
+    opts = {                  -- this table will be passed to setup()
+      ensure_installed = {
+        "c", "cpp", "typescript", "python", "lua", "vim", "vimdoc", "query",
+        "markdown", "markdown_inline",
+      },
+      sync_install = false,
+      auto_install = true,
+      ignore_install = { "javascript" },
+      highlight = {
+        enable = true,
+        disable = { "c", "rust" },
+        additional_vim_regex_highlighting = false,
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
